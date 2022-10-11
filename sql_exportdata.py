8197f8513b35d4df80b9bec04123892f1a0af89b
xwan438 <jocker.xw@gmail.com>
2022-10-11 19:23:53 +0800

    Create sql_exportdata.py

---
+++ sql_exportdata.py
@@ -0,0 +1,21 @@
+import  pymysql
+datalist = []
+db = pymysql.connect(host='20.214.188.216', port=3306, user='root', password='Wx123456.', db='aquadatabase',
+                     charset='utf8')
+cursor = db.cursor()
+
+
+sql = "select temp from web_sc;"
+cursor.execute(sql)
+results = cursor.fetchall()
+
+for i in results:
+
+    for m in i:
+
+        datalist.append(m)
+
+db.commit()
+cursor.close()
+db.close()
+print(datalist)
