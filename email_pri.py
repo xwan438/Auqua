e3d44e753d1f002cb2a18d35d418c260dae0d183
xwan438 <jocker.xw@gmail.com>
2022-10-24 05:03:25 +0800

    Update email_pri.py

--- email_pri.py
+++ email_pri.py
@@ -78,29 +78,9 @@ def message_config(r):
     message['To']   = Header("cutomer", 'utf-8')
     message['Subject'] = Header('data_scraping', 'utf-8')
 
-    file_name = 'temperature_data_test.csv'
-    file_path = os.path.join(file_name)
-    xlsx = MIMEApplication(open(file_path, 'rb').read())
-    xlsx["Content-Type"] = 'application/octet-stream'
-    xlsx.add_header('Content-Disposition', 'attachment', filename=file_name)
-    message.attach(xlsx)
     
-    image1_name = 'ph.jpg'
-    image1_path = os.path.join(image1_name)
     
-    att3=MIMEImage(open(image1_path,'rb').read())
-    att3["Content-Type"]='application/octet-stream'
-    
-    att3.add_header('Content-Disposition', 'attachment', filename=image1_name)
-    message.attach(att3)
-    
-    image2_name = 'temp.jpg'
-    image2_path = os.path.join(image2_name)
-    
-    att2=MIMEImage(open( image2_path,'rb').read())
-    att2["Content-Type"]='application/octet-stream'
-    att2.add_header('Content-Disposition', 'attachment', filename=image2_name)
-    message.attach(att2)
+   
     
     return message
 
