e16a72bc9f36ed24e1ccc0c0932166e70d33e23f
xwan438 <jocker.xw@gmail.com>
2022-10-24 04:59:33 +0800

    Update email_pri.py

--- email_pri.py
+++ email_pri.py
@@ -1,10 +1,25 @@
 from bs4 import BeautifulSoup as bs
 import requests
 import pandas as pd
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.header import Header
+from email.mime.text import MIMEText
+from email.mime.application import MIMEApplication
+from email.mime.image import MIMEImage
+import os
 
 USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Safari/537.36"
 # US english
 LANGUAGE = "en-US,en;q=0.5"
+
+mail_host = "smtp.qq.com"
+mail_user = "1006075208@qq.com"
+mail_pass = "vzgmgbdioztvbcdd"
+
+
+sender = '1006075208@qq.com'
+receivers = ['jocker.xw@gmail.com']
 
 def get_weather_data(url):
     url = f'https://www.google.com/search?q=google+weather+Mahurangi'
@@ -48,6 +63,56 @@ def get_weather_data(url):
     # append to result
     result['next_days'] = next_days
     return result
+def message_config(r):
+
+
+    mail_msg = """
+        <h2>%s</h2>
+        
+        
+    """r
+    content = MIMEText(mail_msg, 'html', 'utf-8')
+    message = MIMEMultipart()
+    message.attach(content)
+    message['From'] = Header("Aqua", 'utf-8')
+    message['To']   = Header("cutomer", 'utf-8')
+    message['Subject'] = Header('data_scraping', 'utf-8')
+
+    file_name = 'temperature_data_test.csv'
+    file_path = os.path.join(file_name)
+    xlsx = MIMEApplication(open(file_path, 'rb').read())
+    xlsx["Content-Type"] = 'application/octet-stream'
+    xlsx.add_header('Content-Disposition', 'attachment', filename=file_name)
+    message.attach(xlsx)
+    
+    image1_name = 'ph.jpg'
+    image1_path = os.path.join(image1_name)
+    
+    att3=MIMEImage(open(image1_path,'rb').read())
+    att3["Content-Type"]='application/octet-stream'
+    
+    att3.add_header('Content-Disposition', 'attachment', filename=image1_name)
+    message.attach(att3)
+    
+    image2_name = 'temp.jpg'
+    image2_path = os.path.join(image2_name)
+    
+    att2=MIMEImage(open( image2_path,'rb').read())
+    att2["Content-Type"]='application/octet-stream'
+    att2.add_header('Content-Disposition', 'attachment', filename=image2_name)
+    message.attach(att2)
+    
+    return message
+
+def send_mail(message):
+
+    try:
+        smtpObj = smtplib.SMTP_SSL(mail_host)
+        smtpObj.login(mail_user, mail_pass)
+        smtpObj.sendmail(sender, receivers, message.as_string())
+
+    except Exception as e:
+        print(e)
     
 
 if __name__ == "__main__":
@@ -65,16 +130,24 @@ if __name__ == "__main__":
     # get data
     data = get_weather_data(URL)
     # print data
-    print("Weather for:", data["region"])
-    print("Now:", data["dayhour"])
-    print(f"Temperature now: {data['temp_now']}°C")
-    print("Description:", data['weather_now'])
-    print("Precipitation:", data["precipitation"])
-    print("Humidity:", data["humidity"])
-    print("Wind:", data["wind"])
-    print("Weather prediction for the next 7 days:")
+    t = ''
+    t_w = ("Weather for:" + data["region"] + "\n")
+    t_d = ("Now:" + data["dayhour"] + "\n")
+    t_t = (f"Temperature now: {data['temp_now']}°C \n")
+    t_d1 = ("Description:" + data['weather_now'] +"\n")
+    t_p = ("Precipitation:" + data["precipitation"] +"\n")
+    t_h = ("Humidity:" +  data["humidity"] + "\n")
+    t_w1 = ("Wind:" +  data["wind"] + "\n")
+    t_wp = ("Weather prediction for the next 7 days:" + "\n")
+    
+    t1 = ''
     for dayweather in data["next_days"]:
-        print("="*40, dayweather["name"], "="*40)
-        print("Description:", dayweather["weather"])
-        print(f"Max temperature: {dayweather['max_temp']}°C")
-        print(f"Min temperature: {dayweather['min_temp']}°C")
+        t1 = t1 + ("="*40 + dayweather["name"] + "="*40 + "\n")
+        t1 = t1 + ("Description:" + dayweather["weather"] + "\n")
+        t1 = t1 + (f"Max temperature: {dayweather['max_temp']}°C \n")
+        t1 = t1 + (f"Min temperature: {dayweather['min_temp']}°C \n")
+    
+    t_r = t + t_1
+    message = message_config(t_r)
+    send_mail(message)
+        
